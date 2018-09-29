2e7a275e740e800e4d42f0894e602dfe2e6fb2c9
Barnabas Agoston <agostonbarna@gmail.com>
2018-09-29 22:07:01 +0200

    Change the location of the release zip's

--- zip.sh
+++ zip.sh
@@ -1,3 +1,11 @@
 #/bin/bash
 
-zip -r TabMarks.zip src
+version=$(git grep -Poh --no-line-number '"version": *"\K[^"]+' src/manifest.json)
+dst=dst/$version.zip
+
+if [[ -f $dst ]]; then
+  echo "$version.zip already exists!"
+  exit 1
+else
+  git archive -v -o dst/$version.zip HEAD:src
+fi
